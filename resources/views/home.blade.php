81a002ff14c4b104fc75560f1f498c1f6bd426b6
Kars <karshamelink97@live.nl>
2020-09-28 20:28:09 +0200

    Admin LTE installed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,20 @@
-@extends('layouts.app')
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+    <p>how to style <a href="https://github.com/jeroennoten/Laravel-AdminLTE/wiki/4.-Usage" target="_blank">here</a></p>
+@stop
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
