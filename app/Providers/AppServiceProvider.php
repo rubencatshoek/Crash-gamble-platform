f6d5ba306b3ae478c8907c22ea4c0005ef0d682d
rubencatshoek <cats0010@hz.nl>
2020-10-12 18:14:35 +0200

    Previous test fix and revert

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if($this->app->environment('production')) {
-            \URL::forceScheme('https');
-        }
+        //
     }
 }
