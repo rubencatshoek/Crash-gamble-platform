603b28e0c6206825ffa8af3750544cc34fc659dc
rubencatshoek <cats0010@hz.nl>
2020-10-29 17:04:00 +0100

    Hide squad and leaderboard if admin role

--- resources/views/layouts/user_menu.blade.php
+++ resources/views/layouts/user_menu.blade.php
@@ -2,11 +2,11 @@
     <h2 class="font-weight-bold pb-2">Menu</h2>
 
     @if($user->isAdmin())
-    <div class="pb-3">
-        <a class="text-decoration-none" href="{{ route('home') }}">
-            Admin panel
-        </a>
-    </div>
+        <div class="pb-3">
+            <a class="text-decoration-none" href="{{ route('home') }}">
+                Admin panel
+            </a>
+        </div>
     @endif
 
     <div class="pb-3">
@@ -15,31 +15,39 @@
         </a>
     </div>
 
-    <div class="pb-3">
-        <a class="text-decoration-none" href="{{ route('squad.index') }}" style="@if(Request::is('squad')) color:white !important; @endif">
-            Squad settings
-        </a>
-    </div>
+    @if(!$user->isAdmin())
+        <div class="pb-3">
+            <a class="text-decoration-none" href="{{ route('squad.index') }}"
+               style="@if(Request::is('squad')) color:white !important; @endif">
+                Squad settings
+            </a>
+        </div>
 
-    @if($user->isLeader())
-    <div class="pb-3">
-        <a class="text-decoration-none" href="{{ route('squad.manage') }}" style="@if(Request::is('manage/squad')) color:white !important; @endif">
-            Manage squad
-        </a>
-    </div>
+        @if($user->isLeader())
+            <div class="pb-3">
+                <a class="text-decoration-none" href="{{ route('squad.manage') }}"
+                   style="@if(Request::is('manage/squad')) color:white !important; @endif">
+                    Manage squad
+                </a>
+            </div>
+        @endif
     @endif
 
     <div class="pb-3">
-        <a class="text-decoration-none" href="{{ route('settings.index') }}" style="@if(Request::is('settings')) color:white !important; @endif">
+        <a class="text-decoration-none" href="{{ route('settings.index') }}"
+           style="@if(Request::is('settings')) color:white !important; @endif">
             Account settings
         </a>
     </div>
 
-    <div class="pb-3">
-        <a class="text-decoration-none" href="{{ route('balance.index') }}" style="@if(Request::is('balance')) color:white !important; @endif">
-            Manage balance
-        </a>
-    </div>
+    @if(!$user->isAdmin())
+        <div class="pb-3">
+            <a class="text-decoration-none" href="{{ route('balance.index') }}"
+               style="@if(Request::is('balance')) color:white !important; @endif">
+                Manage balance
+            </a>
+        </div>
+    @endif
 
     <div class="pb-3">
         <a class="text-decoration-none" href="{{ url('/logout') }}">
