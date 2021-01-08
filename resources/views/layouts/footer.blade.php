0fd0b27a373c09f1218a4dc70d716e3af4c4df69
rubencatshoek <cats0010@hz.nl>
2021-01-08 19:31:19 +0100

    Footer update

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,73 +1,75 @@
-<div class="footer pt-5 pb-3 background-main text-white">
+<div class="footer background-main text-white padding-footer">
     <div class="container">
-        <div class="row">
-            <div class="col-lg-3">
+        <div class="row pb-4">
+            <div class="col-lg-6">
                 <h5 class="font-weight-bold">About us</h5>
 
                 <p class="text-grey">Digital Phase’s gambling provides users with a verifiably safe, anonymous, and
                     exciting experience in the world of online cryptocurrency gambling.</p>
+                <p class="text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
 
-            <div class="col-lg-3">
+            <div class="col-lg-2">
                 <h5 class="font-weight-bold">Quick links</h5>
 
 
                 <ul class="list-unstyled">
                     <li class="pb-1">
-                        <a class="text-grey" href="{{ route('welcome') }}">Home</a>
+                        <a class="text-grey line-height" href="{{ route('welcome') }}">Home</a>
                     </li>
                     <li class="pb-1">
-                        <a class="text-grey" href="{{ route('play.index') }}">Play</a>
+                        <a class="text-grey line-height" href="{{ route('play.index') }}">Play</a>
                     </li>
                     <li class="pb-1">
-                        <a class="text-grey" href="{{ route('leaderboards.index') }}">Leaderboards</a>
+                        <a class="text-grey line-height" href="{{ route('leaderboards.index') }}">Leaderboards</a>
                     </li>
                     <li class="pb-1">
-                        <a class="text-grey" href="{{ route('user.faq.index') }}">FAQ</a>
+                        <a class="text-grey line-height" href="{{ route('user.faq.index') }}">FAQ</a>
                     </li>
                 </ul>
             </div>
 
-            <div class="col-lg-3">
+            <div class="col-lg-2">
                 <h5 class="font-weight-bold">Other links</h5>
 
 
                 <ul class="list-unstyled">
                     <li class="pb-1">
-                        <a class="text-grey" href="{{ route('legal.index') }}">Legal</a>
+                        <a class="text-grey line-height" href="{{ route('legal.index') }}">Legal</a>
                     </li>
                     <li class="pb-1">
-                        <a class="text-grey" href="{{ route('legal.help') }}">Help docs</a>
+                        <a class="text-grey line-height" href="{{ route('legal.help') }}">Help docs</a>
                     </li>
                     <li class="pb-1">
-                        <a class="text-grey" href="{{ route('legal.tos') }}">Terms of Service</a>
+                        <a class="text-grey line-height" href="{{ route('legal.tos') }}">Terms of Service</a>
                     </li>
                 </ul>
             </div>
 
-            <div class="col-lg-3">
+            <div class="col-lg-2">
                 <h5 class="font-weight-bold">Social</h5>
 
 
                 <ul class="list-unstyled">
                     <li class="pb-1">
-                        <a class="text-grey" href="#">Instagram</a>
+                        <a class="text-grey line-height" href="#">Instagram</a>
                     </li>
                     <li class="pb-1">
-                        <a class="text-grey" href="#">Twitter</a>
+                        <a class="text-grey line-height" href="#">Twitter</a>
                     </li>
                     <li class="pb-1">
-                        <a class="text-grey" href="#">Bitcointalk</a>
+                        <a class="text-grey line-height" href="#">Bitcointalk</a>
                     </li>
                     <li class="pb-1">
-                        <a class="text-grey" href="#">Trustpilot</a>
+                        <a class="text-grey line-height" href="#">Trustpilot</a>
                     </li>
                 </ul>
             </div>
         </div>
-        <div class="row pt-3">
+        <hr style="border-top: 1px solid #82828a;" class="mt-5">
+        <div class="row pt-2">
             <div class="col-md-12">
-                <p class="text-center">Copyright © 2020 Crash Gamble Website. All rights reserved.</p>
+                <p class="text-center">Copyright © <?php echo date("Y"); ?> Crash Gamble Website. All rights reserved.</p>
             </div>
         </div>
     </div>
