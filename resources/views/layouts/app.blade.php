499f1ea7d4c83e8041e814e6f2fd9e4cff492702
rubencatshoek <cats0010@hz.nl>
2021-01-10 01:26:26 +0100

    Favicon, SEO description

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,15 +3,19 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name=description
+          content="Digital Phase’s gambling provides users with a verifiably safe, anonymous, and exciting experience in the world of online cryptocurrency gambling.">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Crash Gamble</title>
+    <title>Crash Gamble - Cryptocurrency gambling</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('js/scrolling-nav.js') }}" defer></script>
+
+    <link rel="icon" type="image/png" href="{{ asset('img/logo-small.png') }}">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -131,14 +135,14 @@
                             @endif
 
                             @if(!$user->isAdmin())
-                            <a class="dropdown-item py-2" href="{{ route('profile', $user->name) }}">
-                                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-person-fill"
-                                     fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd"
-                                          d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-                                </svg>
-                                View profile
-                            </a>
+                                <a class="dropdown-item py-2" href="{{ route('profile', $user->name) }}">
+                                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-person-fill"
+                                         fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd"
+                                              d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                                    </svg>
+                                    View profile
+                                </a>
                             @endif
 
                             @if(!$user->isAdmin())
@@ -185,15 +189,17 @@
                                 </a>
                             @endif
 
-                                @if(!$user->isAdmin())
-                                    <a class="dropdown-item py-2" href="{{ route('frontAchievementPage') }}">
+                            @if(!$user->isAdmin())
+                                <a class="dropdown-item py-2" href="{{ route('frontAchievementPage') }}">
 
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
-                                            <path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
-                                        </svg>
-                                        Achievements
-                                    </a>
-                                @endif
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                         class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd"
+                                              d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                                    </svg>
+                                    Achievements
+                                </a>
+                            @endif
 
 
                             <a class="dropdown-item" href="{{ url('/logout') }}">
